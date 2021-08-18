51099cf527871406b9c9aa27e7dbd06f6c0364e7
bharath <bharathkoraboina018@gmail.com>
2021-08-18 19:18:35 +0530

    added file

---
+++ arth.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Purpose:
+#Version:
+#Created Date: Wednesday 11 August 2021 04:13:51 PM IST
+#Modified Date
+#Author: Bharath koraboina
+# START #
+echo "please enter some value: \c"
+read a
+echo "please enter another value: \c"
+read b
+
+echo "a+b value is $(($a+$b))"
+echo "a-b value is $(($a-$b))"
+echo "a*b value is $(($a*$b))"
+echo "a/b value is $(($a/$b))"
+echo "a%b value is $(($a%$b))"
+
+echo "completed succesfully"
+
+# END #
+
